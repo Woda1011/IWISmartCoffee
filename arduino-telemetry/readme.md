2f172ff48dcd5622180556a07f17fd4817020847
Woda <woerner.daniel@gmail.com>
2016-03-22 14:45:38 +0100

    initial sketch for arduino to detect temp and humidity with dht11 sensor and dht library from adafruit

---
+++ arduino-telemetry/readme.md
@@ -0,0 +1,4 @@
+# Setting Up Arduino
+## Setting up IDE
+* Anweisungen auf [Arduino.cc](https://www.arduino.cc/en/Guide/HomePage) befolgen
+* Aktuell kommt ein Arduino Uno R3 zum Einsatz
---
+++ arduino-telemetry/smartCoffee-telemetry/smartCoffee-telemetry.ino
@@ -0,0 +1,34 @@
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+}
