4f32f5db773e01cf916a21f1ad6315002c913bf7
Woda <woerner.daniel@gmail.com>
2016-03-30 17:31:06 +0200

    added json parser to arduino

--- arduino-telemetry/smartCoffee-telemetry/smartCoffee-telemetry.ino
+++ arduino-telemetry/smartCoffee-telemetry/smartCoffee-telemetry.ino
@@ -1,34 +1,43 @@
 #include "DHT.h"
+#include <ArduinoJson.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
+float temperatureToSend;
+float coffeeFillLevelToSend;
+
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("DHT11 test!");
-
+  Serial.println("Telemetry Broker is Running");
   dht.begin();
 }
 
 void loop() {
+  // Read data every 2 seconds
   delay(2000);
 
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
+  // Step 1: Reserve memory space
+  StaticJsonBuffer<200> jsonBuffer;
+  
+  // Step 2: Build object tree in memory
+  JsonObject& root = jsonBuffer.createObject();
+  root["temperature"] = temperatureToSend;
+  root["coffeeFillLevel"] = coffeeFillLevelToSend;
+  
+  float humidityCurrent = dht.readHumidity();
+  float temperatureCurrent = dht.readTemperature();
   
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
+  if (isnan(humidityCurrent) || isnan(temperatureCurrent)) {
+    Serial.println("Failed to Read from!");
     return;
   }
 
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.println(" *C ");
+    temperatureToSend = temperatureCurrent;
+    root["temperature"] = temperatureToSend;
+    root.printTo(Serial);
+  
 }
