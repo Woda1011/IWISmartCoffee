49bfabf558354a0925e05c46c42322faba7dd224
Woda <woerner.daniel@gmail.com>
2016-03-30 17:45:05 +0200

    updated code to avoid sending data all the time. Now the data is only send when it has changed

--- arduino-telemetry/smartCoffee-telemetry/smartCoffee-telemetry.ino
+++ arduino-telemetry/smartCoffee-telemetry/smartCoffee-telemetry.ino
@@ -3,10 +3,10 @@
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
-
 DHT dht(DHTPIN, DHTTYPE);
+
 float temperatureToSend;
-float coffeeFillLevelToSend;
+float humidityToSend;
 
 
 void setup() {
@@ -19,25 +19,27 @@ void loop() {
   // Read data every 2 seconds
   delay(2000);
 
-  // Step 1: Reserve memory space
+  //init json tree
   StaticJsonBuffer<200> jsonBuffer;
-  
-  // Step 2: Build object tree in memory
   JsonObject& root = jsonBuffer.createObject();
   root["temperature"] = temperatureToSend;
-  root["coffeeFillLevel"] = coffeeFillLevelToSend;
-  
+  root["humidity"] = humidityToSend;
+
+  //read sensor data
   float humidityCurrent = dht.readHumidity();
   float temperatureCurrent = dht.readTemperature();
-  
-  // Check if any reads failed and exit early (to try again).
+
   if (isnan(humidityCurrent) || isnan(temperatureCurrent)) {
     Serial.println("Failed to Read from!");
     return;
   }
 
+  //check if the data has changed and prepare json to send
+  if (temperatureCurrent != temperatureToSend || humidityCurrent != humidityToSend) {
     temperatureToSend = temperatureCurrent;
+    humidityToSend = humidityCurrent;
     root["temperature"] = temperatureToSend;
+    root["humidity"] = humidityToSend;
     root.printTo(Serial);
-  
+  }
 }
