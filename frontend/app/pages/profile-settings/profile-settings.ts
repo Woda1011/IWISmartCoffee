73b67e1633eaa9f45aa0254d04a178ab0dbe27c9
Peter Grittner <grpe1012@hs-karlsruhe.de>
2016-04-04 18:18:43 +0200

    fixed: profile-settings - path - bug

--- frontend/app/pages/profile-settings/profile-settings.ts
+++ frontend/app/pages/profile-settings/profile-settings.ts
@@ -2,7 +2,7 @@ import {Page} from 'ionic-angular';
 
 
 @Page({
-  templateUrl: 'build/pages/profile/profile-settings.html'
+  templateUrl: 'build/pages/profile-settings/profile-settings.html'
 })
 export class ProfileSettingsPage {
 
