353f46c9b316d9e71c0a9a04474ac3a1c04c08a6
Markus Klein <klein.markus90@gmx.de>
2016-04-05 17:33:55 +0100

    allowed /api/telemetry and disabled csrf filter for this request

--- backend/src/main/java/de/hska/smartcoffee/security/WebSecurityConfig.java
+++ backend/src/main/java/de/hska/smartcoffee/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package de.hska.smartcoffee.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,11 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
 
 @Configuration
 @EnableWebSecurity
@@ -25,14 +31,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/api/telemetry").permitAll()
+                .antMatchers("/api/telemetry/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .logout().logoutUrl("/api/logout").logoutSuccessHandler(logoutSuccessHandler())
                 .and()
                 .httpBasic().authenticationEntryPoint(authenticationEntryPoint())
                 .and()
-                .csrf().csrfTokenRepository(csrfTokenRepository())
+                .csrf().csrfTokenRepository(csrfTokenRepository()).requireCsrfProtectionMatcher(getRequestMatcher())
                 .and()
                 .addFilterAfter(new CsrfHeaderFilter(), SessionManagementFilter.class)
         ;
@@ -63,5 +69,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private RequestMatcher getRequestMatcher() {
+        return new RequestMatcher() {
+            private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
+
+            @Override
+            public boolean matches(HttpServletRequest request) {
+                // No CSRF due to allowedMethod
+                if (allowedMethods.matcher(request.getMethod()).matches())
+                    return false;
+
+                // No CSRF due to telemetry call
+                if (request.getRequestURI().equals("/api/telemetry"))
+                    return false;
+
+                // CSRF for everything else
+                return true;
+            }
+        };
     }
 }
