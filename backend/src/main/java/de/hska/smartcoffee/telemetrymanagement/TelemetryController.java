f121d274ae44166672e165f214d4dc392416a568
Markus Klein <klein.markus90@gmx.de>
2016-04-05 17:33:02 +0100

    added post interface to push new telemetry data

--- backend/src/main/java/de/hska/smartcoffee/telemetrymanagement/TelemetryController.java
+++ backend/src/main/java/de/hska/smartcoffee/telemetrymanagement/TelemetryController.java
@@ -1,6 +1,7 @@
 package de.hska.smartcoffee.telemetrymanagement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,10 @@ public class TelemetryController {
     @RequestMapping(method = RequestMethod.GET)
     public Telemetry getLatest() {
         return telemetryRepository.findLatestTelemetry();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Telemetry addTelemetry(@RequestBody Telemetry telemetry) {
+        return telemetryRepository.save(telemetry);
     }
 }
