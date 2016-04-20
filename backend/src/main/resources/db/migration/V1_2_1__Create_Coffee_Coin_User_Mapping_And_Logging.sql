537415cba745a59c5dab997926ed7cf8546edb5e
Markus Klein <klein.markus90@gmx.de>
2016-04-20 11:57:44 +0200

    rename table due to class names

--- backend/src/main/resources/db/migration/V1_2_1__Create_Coffee_Coin_User_Mapping_And_Logging.sql
+++ backend/src/main/resources/db/migration/V1_2_1__Create_Coffee_Coin_User_Mapping_And_Logging.sql
@@ -12,12 +12,12 @@ ALTER TABLE user_coffee_coin
 ALTER TABLE user_coffee_coin
   ADD FOREIGN KEY (coffee_coin_id) REFERENCES coffee_coin (id);
 
-CREATE TABLE IF NOT EXISTS cup_log
+CREATE TABLE IF NOT EXISTS coffee_log
 (
   id         BIGINT PRIMARY KEY,
   created_at TIMESTAMP NOT NULL,
   student_id BIGINT    NOT NULL
 );
 
-ALTER TABLE cup_log
+ALTER TABLE coffee_log
   ADD FOREIGN KEY (student_id) REFERENCES student (id);
