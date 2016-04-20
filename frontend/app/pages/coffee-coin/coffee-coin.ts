ed417d47579c4a9d29365da47cbc7be0276b05ef
Markus Klein <klein.markus90@gmx.de>
2016-04-20 17:43:50 +0200

    add select input for coin value and amount

--- frontend/app/pages/coffee-coin/coffee-coin.ts
+++ frontend/app/pages/coffee-coin/coffee-coin.ts
@@ -1,11 +1,26 @@
 import {Page} from 'ionic-angular';
+import {AbstractControl, ControlGroup, FormBuilder, Validators} from "angular2/common";
 
 @Page({
   templateUrl: 'build/pages/coffee-coin/coffee-coin.html'
 })
 export class CoffeeCoin {
 
-  generateCoffeeCoins(){
-    window.location.href = "/api/coffee-coins/generate?coinValue=5&amount=10";
+  coffeeCoinForm: ControlGroup;
+  coinValue: AbstractControl;
+  amount: AbstractControl;
+
+  constructor(private FormBuilder: FormBuilder) {
+    this.coffeeCoinForm = FormBuilder.group({
+      'coinValue': ['10', Validators.compose([Validators.required])],
+      'amount': ['10', Validators.compose([Validators.required])]
+    });
+    this.coinValue = this.coffeeCoinForm.controls['coinValue'];
+    this.amount = this.coffeeCoinForm.controls['amount'];
+  }
+
+  generateCoffeeCoins(formData) {
+    window.location.href = "/api/coffee-coins/generate?coinValue=" + formData.coinValue + "&amount=" + formData.amount;
   }
 }
+
